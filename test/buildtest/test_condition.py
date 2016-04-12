ab0144c7a0166b21fd9bff4ccc839015d7cb5ee1
lipixun <lipixun@outlook.com>
2016-04-12 19:48:11 +0800

    Change the dump and load method for conditions

---
+++ test/buildtest/test_condition.py
@@ -0,0 +1,36 @@
+# encoding=utf8
+
+""" Test the condition
+    Author: lipixun
+    Created Time : 二  4/12 19:42:53 2016
+
+    File Name: test_condition.py
+    Description:
+
+"""
+
+from datahub.conditions import loadCondition, AndCondition, KeyValueCondition
+
+def test_condition_basic():
+    """Test the condition basic
+    """
+    js = {
+        'and': {
+            'conditions': [
+                {
+                    'kv': {
+                        'key': 'akey',
+                        'value': 'avalue',
+                        'equals': True
+                    }
+                }
+            ]
+        }
+    }
+    condition = loadCondition(js)
+    assert isinstance(condition, AndCondition)
+    assert len(condition.conditions) == 1
+    assert isinstance(condition.conditions[0], KeyValueCondition)
+    assert condition.conditions[0].key == 'akey' and condition.conditions[0].value == 'avalue'
+    dumpJson = condition.dump()
+    assert dumpJson == js
